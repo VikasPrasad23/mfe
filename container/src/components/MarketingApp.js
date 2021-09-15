295a67dbe54cdf646e09e60bae838428e1c158e1
Vikas Prasad <atvikaspd@yahoo.com>
2021-09-15 17:56:04 -0400

    Added Navigation code

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,11 +1,26 @@
 import { mount } from 'marketing/MarketingApp';
 import React, {useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 //framework agnostic
 export default() => {
     const ref = useRef(null);
+    //this history object is a copy of current browser history
+    const history = useHistory();
+
     useEffect(() => {
-        mount(ref.current);
-    });
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ( { pathname: nextPathname}) => {
+                const { pathname } = history.location;
+                 //this check is to prevent infinite loop of navigation
+                if(pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            }
+        });
+        history.listen(onParentNavigate);
+    }, []);
+    //To limit how often this use effect function actually gets invoked. I'm going to put in a second argument right there of an empty array.
+    //That means only try to run this function when our marketing app component is first rendered to the screen.
     return <div ref={ref} />
 }
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 //Mount function to start up the app
-const mount = (el) => {
-    ReactDOM.render(<App/>, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+    //defaultHistory only avaialble in development mode
+    const history = defaultHistory || createMemoryHistory();
+
+    if(onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history} />, el);
+
+    //add a function for the container (host App) to communicate/update the child App
+    return {
+        //if the parent navigates we call this function
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+            if (pathname != nextPathname) {
+                history.push(nextPathname);
+            }
+        },
+    }
 };
 
 //If we are in development and in isolation,
@@ -14,7 +33,9 @@ if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
 
     if(devRoot) {
-        mount(devRoot);
+        //the 2nd argument is to prevent the undefined error for 'onNavigate'property in App.js
+        // the value can be empty or as below we have set it to create a browser history instance , whenever he called Mt. function in isolation.
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
